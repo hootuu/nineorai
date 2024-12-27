6c3fd4e07687d031704137ff350d6fb14e86691c
Qinn <qinnsuu@gmail.com>
2024-12-27 19:36:32 +0800

    add after balance

--- domains/token.tx.go
+++ domains/token.tx.go
@@ -6,12 +6,13 @@ import (
 )
 
 type TokenTx struct {
-	Signature Signature    `json:"signature" bson:"signature"`
-	Mint      keys.Address `json:"mint" bson:"mint"`
-	Time      time.Time    `json:"time" bson:"time"`
-	Action    string       `json:"action" bson:"action"`
-	From      keys.Address `json:"from" bson:"from"`
-	To        keys.Address `json:"to" bson:"to"`
-	Amount    uint64       `json:"amount" bson:"amount"`
-	Memo      Memo         `json:"memo" bson:"memo"`
+	Signature    Signature    `json:"signature" bson:"signature"`
+	Mint         keys.Address `json:"mint" bson:"mint"`
+	Time         time.Time    `json:"time" bson:"time"`
+	Action       string       `json:"action" bson:"action"`
+	From         keys.Address `json:"from" bson:"from"`
+	To           keys.Address `json:"to" bson:"to"`
+	Amount       uint64       `json:"amount" bson:"amount"`
+	Memo         Memo         `json:"memo" bson:"memo"`
+	AfterBalance uint64       `json:"after_balance" bson:"after_balance"`
 }
